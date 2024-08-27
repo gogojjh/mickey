54faac24eb19829b1e0fa5eb95edd33f234eaf63
gogojjh <jiaojh1994@gmail.com>
2024-08-27 22:27:07 +0100

    add new note in benchmarking

--- demo_inference.py
+++ demo_inference.py
@@ -1,3 +1,12 @@
+"""
+Usage: python demo_inference.py \
+--im_path_ref data/toy_example/im0.jpg \
+--im_path_dst data/toy_example/im1.jpg \
+--intrinsics data/toy_example/intrinsics.txt \
+--checkpoint path/to/checkpoint \
+--config path/to/config
+"""
+
 import torch
 import argparse
 from lib.models.builder import build_model
@@ -8,6 +17,7 @@ import numpy as np
 from pathlib import Path
 import cv2
 
+
 def prepare_score_map(scs, img, temperature=0.5):
 
     score_map = generate_heat_map(scs, img, temperature)
@@ -16,23 +26,47 @@ def prepare_score_map(scs, img, temperature=0.5):
 
     return score_map
 
-def colorize_depth(value, vmin=None, vmax=None, cmap='magma_r', invalid_val=-99, invalid_mask=None, background_color=(0, 0, 0, 255), gamma_corrected=False, value_transform=None):
 
-    img = colorize(value, vmin, vmax, cmap, invalid_val, invalid_mask, background_color, gamma_corrected, value_transform)
+def colorize_depth(
+    value,
+    vmin=None,
+    vmax=None,
+    cmap="magma_r",
+    invalid_val=-99,
+    invalid_mask=None,
+    background_color=(0, 0, 0, 255),
+    gamma_corrected=False,
+    value_transform=None,
+):
+
+    img = colorize(
+        value,
+        vmin,
+        vmax,
+        cmap,
+        invalid_val,
+        invalid_mask,
+        background_color,
+        gamma_corrected,
+        value_transform,
+    )
 
     shape_im = img.shape
     img = np.asarray(img, np.uint8)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-    img = cv2.resize(img, (shape_im[1]*14, shape_im[0]*14), interpolation=cv2.INTER_LINEAR)
+    img = cv2.resize(
+        img, (shape_im[1] * 14, shape_im[0] * 14), interpolation=cv2.INTER_LINEAR
+    )
 
     return img
+
 
 def read_color_image(path, resize):
     """
     Args:
-        resize (tuple): align image to depthmap, in (w, h).
+            resize (tuple): align image to depthmap, in (w, h).
     Returns:
-        image (torch.tensor): (3, h, w)
+            image (torch.tensor): (3, h, w)
     """
     # read and resize image
     cv_type = cv2.IMREAD_COLOR
@@ -46,14 +80,15 @@ def read_color_image(path, resize):
 
     return image.unsqueeze(0)
 
+
 def read_intrinsics(path_intrinsics, resize):
     Ks = {}
-    with Path(path_intrinsics).open('r') as f:
+    with Path(path_intrinsics).open("r") as f:
         for line in f.readlines():
-            if '#' in line:
+            if "#" in line:
                 continue
 
-            line = line.strip().split(' ')
+            line = line.strip().split(" ")
             img_name = line[0]
             fx, fy, cx, cy, W, H = map(float, line[1:])
 
@@ -63,13 +98,14 @@ def read_intrinsics(path_intrinsics, resize):
             Ks[img_name] = K
     return Ks
 
+
 def run_demo_inference(args):
 
     # Select device
     use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    print('Preparing data...')
+    print("Preparing data...")
 
     # Prepare config file
     cfg.merge_from_file(args.config)
@@ -86,46 +122,107 @@ def run_demo_inference(args):
 
     # Prepare data for MicKey
     data = {}
-    data['image0'] = im0
-    data['image1'] = im1
-    data['K_color0'] = torch.from_numpy(K['im0.jpg']).unsqueeze(0).to(device)
-    data['K_color1'] = torch.from_numpy(K['im1.jpg']).unsqueeze(0).to(device)
+    data["image0"] = im0
+    data["image1"] = im1
+    data["K_color0"] = torch.from_numpy(K["im0.jpg"]).unsqueeze(0).to(device)
+    data["K_color1"] = torch.from_numpy(K["im1.jpg"]).unsqueeze(0).to(device)
 
     # Run inference
-    print('Running MicKey relative pose estimation...')
+    print("Running MicKey relative pose estimation...")
     model(data)
 
-    # Pose, inliers and score are stored in:
-    # data['R'] = R
-    # data['t'] = t
-    # data['inliers'] = inliers
-    # data['inliers_list'] = inliers_list
+    ############################## DEBUG
+    # Explanation of data['']
+    R = data["R"]
+    t = data["t"]
+    print("Estimated Poses:\n", R, "\n", t)
+    print(data.keys())
 
-    print('Saving depth and score maps in image directory ...')
-    depth0_map = colorize_depth(data['depth0_map'][0], invalid_mask=(data['depth0_map'][0] < 0.001).cpu()[0])
-    depth1_map = colorize_depth(data['depth1_map'][0], invalid_mask=(data['depth1_map'][0] < 0.001).cpu()[0])
-    score0_map = prepare_score_map(data['scr0'][0], data['image0'][0], temperature=0.5)
-    score1_map = prepare_score_map(data['scr1'][0], data['image1'][0], temperature=0.5)
+    kpts0, kpts1 = data["kps0"].squeeze(0).T, data["kps1"].squeeze(0).T
+    desc0, desc1 = data["dsc0"].squeeze(0).T, data["dsc1"].squeeze(0).T
+    print(data["inliers_list"][0].shape)
+    num_inliers = data["inliers"].cpu().numpy().flatten()[0]
+    inliers_list = data["inliers_list"][0].cpu().numpy()
+    confidence = num_inliers / 2048
+    print(inliers_list)
+    print(f"{confidence=}, {num_inliers=}, {inliers_list.shape=}")
+    print(data["final_scores"])
+    input()
 
-    ext_im0 = args.im_path_ref.split('.')[-1]
-    ext_im1 = args.im_path_dst.split('.')[-1]
+    mkpts0 = inliers_list[:, :2]
+    mkpts1 = inliers_list[:, 2:4]
 
-    cv2.imwrite(args.im_path_ref.replace(ext_im0, 'score.jpg'), score0_map)
-    cv2.imwrite(args.im_path_dst.replace(ext_im1, 'score.jpg'), score1_map)
+    import sys
 
-    cv2.imwrite(args.im_path_ref.replace(ext_im0, 'depth.jpg'), depth0_map)
-    cv2.imwrite(args.im_path_dst.replace(ext_im1, 'depth.jpg'), depth1_map)
+    sys.path.append(
+        "/Titan/code/robohike_ws/src/image-matching-models/third_party/LightGlue"
+    )
+    from lightglue import viz2d  # for quick import later 'from matching import viz2d'
 
-if __name__ == '__main__':
+    with torch.no_grad():
+        viz2d.plot_images([im0.squeeze(0), im1.squeeze(0)])
+        viz2d.plot_matches(mkpts0[::100, :], mkpts1[::100, :], color="lime", lw=1.5)
+        viz2d.add_text(0, f"{len(mkpts1)} matches", fs=20)
+        viz_path = "data/toy_example/img_matches.jpg"
+        viz2d.save_plot(viz_path)
+
+    input()
+    ##############################
+
+    print("Saving depth and score maps in image directory ...")
+    depth0_map = colorize_depth(
+        data["depth0_map"][0], invalid_mask=(data["depth0_map"][0] < 0.001).cpu()[0]
+    )
+    depth1_map = colorize_depth(
+        data["depth1_map"][0], invalid_mask=(data["depth1_map"][0] < 0.001).cpu()[0]
+    )
+    score0_map = prepare_score_map(data["scr0"][0], data["image0"][0], temperature=0.5)
+    score1_map = prepare_score_map(data["scr1"][0], data["image1"][0], temperature=0.5)
+
+    ext_im0 = args.im_path_ref.split(".")[-1]
+    ext_im1 = args.im_path_dst.split(".")[-1]
+
+    cv2.imwrite(args.im_path_ref.replace(ext_im0, "score.jpg"), score0_map)
+    cv2.imwrite(args.im_path_dst.replace(ext_im1, "score.jpg"), score1_map)
+
+    cv2.imwrite(args.im_path_ref.replace(ext_im0, "depth.jpg"), depth0_map)
+    cv2.imwrite(args.im_path_dst.replace(ext_im1, "depth.jpg"), depth1_map)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--im_path_ref', help='path to reference image', default='data/toy_example/im0.jpg')
-    parser.add_argument('--im_path_dst', help='path to destination image', default='data/toy_example/im1.jpg')
-    parser.add_argument('--intrinsics', help='path to intrinsics file', default='data/toy_example/intrinsics.txt')
-    parser.add_argument('--resize', nargs=2, type=int, help='resize applied to the image and intrinsics (w, h)', default=None)
-    parser.add_argument('--config', help='path to config file', default='weights/mickey_weights/config.yaml')
-    parser.add_argument('--checkpoint', help='path to model checkpoint',
-                        default='weights/mickey_weights/mickey.ckpt')
+    parser.add_argument(
+        "--im_path_ref",
+        help="path to reference image",
+        default="data/toy_example/im0.jpg",
+    )
+    parser.add_argument(
+        "--im_path_dst",
+        help="path to destination image",
+        default="data/toy_example/im1.jpg",
+    )
+    parser.add_argument(
+        "--intrinsics",
+        help="path to intrinsics file",
+        default="data/toy_example/intrinsics.txt",
+    )
+    parser.add_argument(
+        "--resize",
+        nargs=2,
+        type=int,
+        help="resize applied to the image and intrinsics (w, h)",
+        default=None,
+    )
+    parser.add_argument(
+        "--config",
+        help="path to config file",
+        default="weights/mickey_weights/config.yaml",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        help="path to model checkpoint",
+        default="weights/mickey_weights/mickey.ckpt",
+    )
     args = parser.parse_args()
 
     run_demo_inference(args)
-
